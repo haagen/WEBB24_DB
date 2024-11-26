d03101beaf20d38be129898c8ea40569e4fe6073
Martin Haagen <martin@mbh.se>
2024-11-26 11:59:56 +0100

    Lektion 1 - Intro och Hello World

---
+++ src/Lektion01/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lektion 1 - Hello World</title>
+</head>
+<body>
+
+    <h1>Lektion 1 - Hello World</h1>
+
+    <!-- En html-kommentar -->
+
+    <?php
+
+        // Vår första kod i PHP
+        echo 'Hello, World!';
+        
+        /*
+            Vi kan skriva flera rader med kommentarer!
+        */
+
+    ?>
+
+</body>
+</html>
